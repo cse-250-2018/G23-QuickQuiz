26145002e0742373d3f270edfa78af8c070f57a4
SHRony <shrony101@gmail.com>
2021-11-21 19:31:05 +0600

    add create exam page

---
+++ create_exam.php
@@ -0,0 +1,41 @@
+<?php
+    include 'connection.php';
+    if(!isset($_SESSION['current_user']))
+    {
+        header("Location:login.php");
+        die;
+    }
+?>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Raiden starts developing</title>
+        <link rel="stylesheet"href="css/style.css">
+        <link rel="stylesheet" href="css/exam.css">
+        <script src="scripts/vanila.js"></script>
+        <script src="scripts/exam.js"></script>
+        <div ></div>
+    </head>
+    <body onload="getQuestion()">
+        <?php include 'parts/nav_bar.php' ?>
+        <div id="home">
+            <div id="exam_name"><input type="text" placeholder="Exam Title" required></div>
+            <div id="input_time">
+                <div>Start:
+                    <input type="datetime-local" required>
+                </div>
+                <div>
+                    End:
+                    <input type="datetime-local" required>
+                </div>
+            </div>
+            <div id="questions_container">
+                
+            </div>
+            <div id="question_menu">
+                <div id="add_question" onclick="getQuestion()">+Add Question</div>
+                <div id="done" onclick="submit()">Create Exam</div>
+            </div>
+        </div>
+    </body>
+</html>
