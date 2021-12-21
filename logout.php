2825e2d5aa8baa3d01ad57b7dfd1fa18631953e0
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2021-12-21 14:36:13 +0600

    remove note sorting history

--- logout.php
+++ logout.php
@@ -1,10 +1,30 @@
 <?php
+    
+    session_start();
 
-	session_start();
 	if(isset($_SESSION['current_user']))
 	{
 		unset($_SESSION['current_user']);
 	}
+    
+    if(isset($_SESSION['A_type']))
+	{
+		unset($_SESSION['A_type']);
+	}
+    
+    if(isset($_SESSION['N_type']))
+	{
+		unset($_SESSION['N_type']);
+	}
+    if(isset($_SESSION['C_type']))
+	{
+		unset($_SESSION['C_type']);
+	}
+    if(isset($_SESSION['T_type']))
+	{
+		unset($_SESSION['T_type']);
+	}
+
 
 	header("Location: index.php");
 	die;
