908611c02ef0943d6711174e30ba44f381d6eff0
SHRony <shrony101@gmail.com>
2021-12-24 14:28:04 +0600

    Improved UI for homepage and forms

--- login.php
+++ login.php
@@ -36,31 +36,32 @@
 <head>
 	<title> LogIn form </title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/form.css">
 </head>
 
 <body>
 	<?php include 'parts/nav_bar.php' ?>
-	<div class="box-2">
-	<h1>Log in</h1><div class="bar"></div>
-	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" class="my-form">
-		<?php echo $msg; ?>
-		
-		<div class="form-group">
-			<label>Username</label>
-            <input type="text" name="username" value="<?php echo $_SESSION['f_user_name']; ?>">
-        </div>
-		
-        <div class="form-group">
-			<label>Password</label>
-            <input type="password" name="password">
-        </div>
-		
-        <button type="submit" name="submit" class="button">Log in</button>
-       
-		<p> Don't have an account? <a href="registration.php">Register here</a><br/>
-		Forget password? <a href="reset.php">Reset</a></p>
-		
-    </form>
+	<div id="container">
+		<div id="form_total">
+				<div id="form_lft">
+					<div id="lft1">
+						<img src="images/form_left.svg">
+											</div>
+					<div id="lft2"><img src="images/form_right.svg"></div>
+				</div>
+				<form id="form_rht" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+				<?php echo $msg; ?>
+					<div id="form_title">Sign In</div>
+					<label class="inp_lbl">Username</label>
+					<input class="inp" type="text" name="username" value="<?php echo $_SESSION['f_user_name']; ?>" >
+					<label class="inp_lbl">Password</label>
+					<input class="inp" type="password" name="password">
+					<div class="inp_btn_container"><button class="inp_btn" type="submit" name="submit">Log In</button></div>
+					<div class="form_extra">
+						Don't have an account? <a href="registration.php">Sign Up</a>
+					</div>
+				</form>
+			</div>
 	</div>
 </body>
 
--- registration.php
+++ registration.php
@@ -28,45 +28,45 @@
 <head>
 	<title> Registration form </title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/form.css">
 </head>
 
 <body>
 	<?php include 'parts/nav_bar.php' ?>
-	<div class="box-2">
-	<h1>Create a new account</h1><div class="bar"></div>
-	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" class="my-form">
-		<?php echo $msg; ?>
-		<div class="form-group">
-			<label>Name</label>
-            <input type="text" name="name" value="<?php echo $_SESSION['f_name']; ?>">
-        </div>
-		
-		<div class="form-group">
-			<label>Username</label>
-            <input type="text" name="username" value="<?php echo $_SESSION['f_user_name']; ?>">
-        </div>
-        
-		<div class="form-group">
-			<label>Email</label>
-            <input type="email" name="email" value="<?php echo $_SESSION['f_email']; ?>">
-        </div>
-		
-        <div class="form-group">
-			<label>Password</label>
-            <input type="password" name="password">
-        </div>
-		 
-		<div class="form-group">
-			<label>Confirm Password</label>
-            <input type="password" name="cpassword">
-        </div>
-		
-        <button type="submit" name="submit" class="button">Submit</button>
-       
-		<p> Already have an account? <a href="login.php">Log in</a></p>
-		
-    </form>
-	</div>
+	<div id="container">
+        <div id="form_total">
+                <div id="form_lft">
+                    <div id="lft1">
+                        <img src="images/form_left.svg">
+                                            </div>
+                    <div id="lft2"><img src="images/form_right.svg"></div>
+                </div>
+                <form id="form_rht" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+                    <div id="form_title">Join Us</div>
+                    <label class="inp_lbl">Username</label>
+                    <input class="inp" type="text" name="username" value="<?php echo $_SESSION['f_user_name']; ?>" >
+                    
+                    <label class="inp_lbl">Full Name</label>
+                    <input class="inp" type="text"  name="name" value="<?php echo $_SESSION['f_name']; ?>">
+                    
+                    <label class="inp_lbl">Email</label>
+                    <input class="inp" type="text" name="email" value="<?php echo $_SESSION['f_email']; ?>">
+                    
+                    <label class="inp_lbl">Password</label>
+                    <input class="inp" type="password" name="password">
+                    
+                    <label class="inp_lbl">Confirm Password</label>
+                    <input class="inp" type="password" name="cpassword">
+                    
+                    <label class="inp_lbl">Registration Number</label>
+                    <input class="inp" type="text">
+                    <div class="inp_btn_container"><button class="inp_btn" type="submit" name="submit">Create Account</button></div>
+                    <div class="form_extra">
+                        Already have an account? <a href="login.php">Sign In</a>
+                    </div>
+                </form>
+            </div>
+    </div>
 </body>
 
 </html>
