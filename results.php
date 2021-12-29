81444c9351e03378b53c325ff395e3c43908616a
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2021-12-30 02:12:04 +0600

    functionality for quiz & link to details of result

--- results.php
+++ results.php
@@ -8,7 +8,7 @@
         <title>Raiden starts developing</title>
         <link rel="stylesheet"href="css/style.css">
         <link rel="stylesheet" href="css/exam.css">
-        <script src="scripts/run.js"></script>
+        <script src="scripts/vanila.js"></script>
         <script src="scripts/exam.js"></script>
         <div ></div>
     </head>
@@ -30,7 +30,25 @@
                                         
                                         echo '<div class="score_container">';
                                             echo '<div class="score_user">'.$row["user"].'</div>';
-                                            echo '<div class="score">'.$row["score"].'</div>';
+                                            echo '<div class="score"><a href="resultDetails.php?exam='.$_GET["examid"].'&user='.$row["user"].'">'.$row["score"].'</a></div>';
+                                        echo '</div>';
+                                    }
+                                }
+                            }
+                            else if(isset($_GET['quizid'])){
+                                $query = "SELECT * FROM results WHERE quiz = ".$_GET["quizid"]." ORDER BY score DESC ";
+                                $result = mysqli_query($con,$query);
+                                if ($result){
+                                    $sql="SELECT * FROM quizs WHERE id = '".$_GET["quizid"]."'";
+                                    $res=mysqli_query($con,$sql);
+                                    $quiz=mysqli_fetch_array($res);
+                                    echo '<h2>Results of '.$quiz["name"].'</h2>';
+                                    while($row = mysqli_fetch_array($result))
+                                    {
+                                        
+                                        echo '<div class="score_container">';
+                                            echo '<div class="score_user">'.$row["user"].'</div>';
+                                            echo '<div class="score"><a href="resultDetails.php?quiz='.$_GET["quizid"].'&user='.$row["user"].'">'.$row["score"].'</a></div>';
                                         echo '</div>';
                                     }
                                 }
