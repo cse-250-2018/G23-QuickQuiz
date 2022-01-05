c29d1d7acf092ada90f02559f24947b793d5780e
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2022-01-05 22:08:56 +0600

    UI improve

--- notes.php
+++ notes.php
@@ -10,61 +10,59 @@
     <head>
         
         <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/form.css">
         <link rel="stylesheet" href="css/notes.css">
     </head>
-    <body>
+    <body style="background-image: url(images/form_page_bg.svg);">
         <?php include 'parts/nav_bar.php' ?>
+        
+        <div id="container" style="min-height:0; padding:5px;">
+            <div id="form_total">
+                <div id="form_lft">
+                    <div id="lft1">
+                        <img src="images/file2.png" width="370" height="490">
+                                            </div>
+                    <div id="lft2"><img src="images/form_right.svg"></div>
+                </div>
+
+                <form id="form_rht" action="upload.php" method="POST" enctype="multipart/form-data">
+                    <div id="form_title">Upload File</div>
+                    <label class="inp_lbl">Course Name:</label>
+                    <select class="inp" name="course"> 
+                        <option value="Structured Programming Language">Structured Programming Language</option>
+                        <option value="Discrete Math">Discrete Math</option>
+                        <option value="Data Structures">Data Structures</option>
+                        <option value="Algorithm Design & Analysis">Algorithm Design & Analysis</option>
+                        <option value="Object Oriented Programming">Object Oriented Programming </option>
+                        <option value="Numerical Analysis">Numerical Analysis </option>
+                        <option value="Theory of Computation">Theory of Computation</option>
+                        <option value="Ethics & Cyber Law"> Ethics & Cyber Law </option>
+                        <option value="Digital Signal Processing">Digital Signal Processing </option>
+                        <option value="Database System"> Database System </option>
+                        <option value="Operating System"> Operating System </option>
+                        <option value="Computer Networking"> Computer Networking </option>
+                        <option value="Computer Graphics"> Computer Graphics </option>
+                        <option value="Computer Architecture"> Computer Architecture </option>
+                        <option value="Artificial Intelligence"> Artificial Intelligence </option>
+                        <option value="Machine Learning"> Machine Learning </option>
+                        <option value="Others"> Others </option>
+
+                    </select>
+                    <label class="inp_lbl">Type:</label>
+                    <select class="inp" name="type">
+                        <option value="public">Public</option>
+                        <option value="private">Private</option>
+                    </select>
+                    <label class="inp_lbl">Note(Only pdf):</label>
+                    <input type="file" class="inp" id="choose" name="file" accept=".pdf" required />
+                    
+                    <input type="hidden" name="author" value="<?php echo $_SESSION['current_user'] ?>">
+                    <input class="inp_btn" type="submit" name="submit" value="Upload">
+                </form>
+            </div>  
+        </div>
         <div id="center">
             <div class="notes_home_container">
-                
-                
-                <div class="box-2">
-                    <h1 id="heading">Upload File</h1>
-                    <form method="POST" enctype="multipart/form-data" action="upload.php">
-                        <div class="form-group">
-                            <label>Course Name:</label>
-                            <select name="course"> 
-                                
-                                <option value="Structured Programming Language">Structured Programming Language</option>
-                                <option value="Discrete Math">Discrete Math</option>
-                                <option value="Data Structures">Data Structures</option>
-                                <option value="Algorithm Design & Analysis">Algorithm Design & Analysis</option>
-                                <option value="Object Oriented Programming">Object Oriented Programming </option>
-                                <option value="Numerical Analysis">Numerical Analysis </option>
-                                <option value="Theory of Computation">Theory of Computation</option>
-                                <option value="Ethics & Cyber Law"> Ethics & Cyber Law </option>
-                                <option value="Digital Signal Processing">Digital Signal Processing </option>
-                                <option value="Database System"> Database System </option>
-                                <option value="Operating System"> Operating System </option>
-                                <option value="Computer Networking"> Computer Networking </option>
-                                <option value="Computer Graphics"> Computer Graphics </option>
-                                <option value="Computer Architecture"> Computer Architecture </option>
-                                <option value="Artificial Intelligence"> Artificial Intelligence </option>
-                                <option value="Machine Learning"> Machine Learning </option>
-                                <option value="Others"> Others </option>
-                
-                            </select>
-                            </div>
-                        
-                        
-                        <div class="form-group">
-                            <label>Type:</label>
-                            <select name="type">
-                                <option value="public">Public</option>
-                                <option value="private">Private</option>
-                            </select>
-                        </div>
-                        
-                        <div class="form-group">
-                            <label>Note(Only pdf):</label>
-                            <input type="file" class="choose" name="file" accept=".pdf" required />
-                        </div>
-                        
-                        <input type="hidden" name="author" value="<?php echo $_SESSION['current_user'] ?>">
-                        <input id="button" type="submit" name="submit" value="Upload">
-                </form>
-        
-            </div>
                 
                 <?php
     
