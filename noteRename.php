e92457444844a9a9efec04c928696c786ffdaac8
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2022-01-05 20:17:04 +0600

    Update noteRename.php

--- noteRename.php
+++ noteRename.php
@@ -48,7 +48,59 @@
         echo "old=$old <br> new=$new";
     }
 
+    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit']))
+	{
+        
+        $id = $_POST['id']; 
+		$new_course = $_POST['course'];
+        $new_type = $_POST['type'];
+        $new_name = $_POST['fileName'];
+        
+        
+        $sql = "SELECT * FROM notes WHERE ID = '$id' ";
+        $result = mysqli_query($con, $sql);
 
-    //header("Location: profile.php");
+        if(mysqli_num_rows($result) > 0)
+        {	
+            $row = mysqli_fetch_assoc($result);
+            $author=$row['Author'];
+            $name=$row['Name'];
+            $folder = "notes/".$_SESSION['current_user'];
+            $old = $folder."/".$name;
+            $new = $folder."/".$new_name;
+            
+            //in local
+            if(!file_exists($new))
+            {
+                if(rename($old, $new))
+                    echo "Changed";
+                else{
+                    $_SESSION['msg']="Something error";
+                    header("Location: noteEdit?id=$id");
+                    die;
+                }
+            }
+            else if($old != $new)
+            {
+                echo "Same nam ache!";
+                $_SESSION['msg']="This name already exists";
+                header("Location: noteEdit?id=$id");
+                die;
+            }
+
+            //in database
+            $sql2="UPDATE `notes` SET `Name`='".$new_name."',`Course`='".$new_course."',`Type`='".$new_type."' WHERE `ID` = '".$id."'";
+            mysqli_query($con, $sql2);
+            
+        }
+        
+        /*echo "<br>id=$id new-name=$new_name<br>";
+        echo "old=$old <br> new=$new";
+        echo $new_course."  ".$new_type;*/
+		
+	}
+
+    //$_SESSION['msg']
+    header("Location: profile.php");
 
 ?>
